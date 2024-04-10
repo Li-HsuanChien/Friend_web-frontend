b79d41b04297d8d8d01598c3a9ab8137ec08d0e4
Li-HsuanChien <shanechien2023@gmail.com>
2024-04-09 20:26:32 -0400

    add # to domain

--- src/lib/domainList.ts
+++ src/lib/domainList.ts
@@ -1,4 +1,4 @@
 export const backendurl = 'https://friend-web.net/';
 
-export const fronturl = 'https://li-hsuanchien.github.io/Friend_web-frontend/';
+export const fronturl = 'https://li-hsuanchien.github.io/Friend_web-frontend/#/';
 
